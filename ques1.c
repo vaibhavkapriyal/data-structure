563aca7cce1980785fd9384206d5fb6413d34db6
vaibhavkapriyal <91269085+vaibhavkapriyal@users.noreply.github.com>
2021-12-10 10:40:53 +0530

    Update ques1.c

--- ques1.c
+++ ques1.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- int main()
- {
-	int n,i,t=0,c=0;
+void check(int *a,int n)
+{
+	int i,t=0,c=0;
+	for(i=0;i<n;i++)
+	{
+		t=0;
+		for(int j=0;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+				t++;
+			}	
+		}
+		if(t==1)
+		{
+			c++;
+		}
+		if(c==2)
+		{
+			printf("Second non repeating elements = %d\n",a[i]);
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int n,i;
 	printf("Enter the length of the array\n");
 	scanf("%d",&n);
 	int a[n];
@@ -12,23 +37,7 @@
 	{
 		scanf("%d",&a[i]);
 	}
-	for(i=0;i<n;i++)
-	{
-		for(int j=0;j<n;j++)
-		{
-			if(a[i]==a[j])
-				t++;	
-		}
-		if(t==1)
-		{
-			c++;
-		}
-		if(c==2)
-			{
-				printf("Second non repeating elements = %d\n",a[i]);
-				break;
-			}
-	}
+	check(a,n);
 	return 0;
 }
  
